c59c9d88b00bde8d5903b9fff50e79ca5ac0979e
Jordan Skophammer <33002058+jordan-skophammer@users.noreply.github.com>
2019-06-10 12:41:51 -0500

    sandbox commit
    changed state structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,123 +1,139 @@
-import React, {Component} from "react";
-import Grid from '@material-ui/core/Grid';
-import Icon from '@material-ui/core/Icon';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import Chart from '../components/chart';
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Icon from "@material-ui/core/Icon";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Chart from "../components/chart";
 
 //Styling
 const centerItem = {
-    textAlign: 'center'
-}
+  textAlign: "center"
+};
 
 // const addInput = () => {
 //     console.log("clicked")
 // }
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            inputs: [
-                {
-                    label: 'label 1',
-                    amount: 'amount 1'
-                }
-            
-            ]
-        }
-
-        this.changeAmount = this.changeAmount.bind(this)
-    }
-    
-
-    componentDidMount() {
-        this.getDataFromDb();
-    }
-
-    getDataFromDb = () => {
-        fetch("/api/getData")
-            // .then(data => data.json())
-            .then(res => this.setState({ data: res.data }));
+    this.state = {
+      items: {
+        label: 1,
+        label2: 2
+      }
     };
 
-    // addInput(event) {
-    //     new inputObject() 
-        
-    //     this.setState(prevState => ({ inputs: prevState.inputs.concat([newInput])}))
+    // this.changeAmount = this.changeAmount.bind(this)
+  }
 
-    //     console.log(event.target.id)
-    // }
+  componentDidMount() {
+    this.getDataFromDb();
+  }
 
-    changeAmount(event) {
-        let newAmount = event.target.value
+  getDataFromDb = () => {
+    fetch("/api/getData")
+      // .then(data => data.json())
+      .then(res => this.setState({ data: res.data }));
+  };
 
-        // this.setState({...this.state, inputs: {amount: newVaule}})
+  // addInput(event) {
+  //     new inputObject()
 
-        console.log(newAmount)
-    }
+  //     this.setState(prevState => ({ inputs: prevState.inputs.concat([newInput])}))
 
-    // totalSum = () => {
+  //     console.log(event.target.id)
+  // }
 
-    // }
+  changeAmount(event) {
+    let newAmount = event.target.value;
 
-    render() {
-        return(
-                <Grid container style={centerItem} direction="row" justify="space-between" alignItems="center" spacing={16}>
-                    {/* Navbar */}
-                    <Grid item xs={2}><Icon>home</Icon></Grid>
-                    <Grid item xs={6}><Paper><h3>Balance</h3></Paper></Grid>
-                    <Grid item xs={2}><Icon>perm_identity</Icon></Grid>
-                    
-                    {/* Totals Chart */}
-                    {/* <Grid item xs={12}><Chart /></Grid> */}
+    // this.setState({...this.state, inputs: {amount: newVaule}})
 
-                    {/* Menu Item */}
-                    <Grid container style={centerItem} alignItems="center" item xs={12}>
-                        <Paper>Expenses</Paper>
-                        <div>
-                            {this.state.inputs.map(id => 
-                            <Grid item xs={12} key={id}>
-                                <TextField onChange={this.changeAmount}
-                                className="label"
-                                placeholder="Label"
-                                direction="row"
-                                xs={12}
-                                id="outlined-bare"
-                                defaultValue=""
-                                margin="normal"
-                                variant="outlined"
-                                inputProps={{ 'aria-label': 'bare' }}
-                                />
-                                <TextField onChange={this.changeAmount}
-                                placeholder="Amount"  
-                                direction="row"
-                                xs={12}
-                                id="outlined-bare"
-                                defaultValue=""
-                                margin="normal"
-                                variant="outlined"
-                                InputProps={{
-                                startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                                }}
-                                />
-                            </Grid>)}
-                        </div>
-                        <Button variant="contained" color="primary" onClick={this.addInput}>Add</Button>
-                    </Grid>
-                    <Grid item xs={12}><Paper>Total = </Paper></Grid>
-                    {/* <Grid item xs={12}><Paper>Savings</Paper></Grid>
+    console.log(newAmount);
+  }
+
+  // totalSum = () => {
+
+  // }
+
+  render() {
+    return (
+      <Grid
+        container
+        style={centerItem}
+        direction="row"
+        justify="space-between"
+        alignItems="center"
+        spacing={16}
+      >
+        {/* Navbar */}
+        <Grid item xs={2}>
+          <Icon>home</Icon>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>
+            <h3>Balance</h3>
+          </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Icon>perm_identity</Icon>
+        </Grid>
+
+        {/* Totals Chart */}
+        {/* <Grid item xs={12}><Chart /></Grid> */}
+
+        {/* Menu Item */}
+        <Grid container style={centerItem} alignItems="center" item xs={12}>
+          <Paper>Expenses</Paper>
+          <div>
+            {Object.keys(this.state.items).map(item => (
+              <Grid item xs={12} key={item}>
+                <TextField
+                  onChange={this.changeAmount}
+                  className="label"
+                  placeholder={item}
+                  direction="row"
+                  xs={12}
+                  id="outlined-bare"
+                  defaultValue=""
+                  margin="normal"
+                  variant="outlined"
+                  inputProps={{ "aria-label": "bare" }}
+                />
+                <TextField
+                  onChange={this.changeAmount}
+                  placeholder={this.state.items[item]}
+                  direction="row"
+                  xs={12}
+                  id="outlined-bare"
+                  margin="normal"
+                  variant="outlined"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">$</InputAdornment>
+                    )
+                  }}
+                />
+              </Grid>
+            ))}
+          </div>
+          <Button variant="contained" color="primary" onClick={this.addInput}>
+            Add
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper>Total = </Paper>
+        </Grid>
+        {/* <Grid item xs={12}><Paper>Savings</Paper></Grid>
                     <Grid item xs={12}><Paper>Debt</Paper></Grid>
                     <Grid item xs={12}><Paper>Assets</Paper></Grid> */}
-                </Grid>
-            
-        )
-        
-    }
+      </Grid>
+    );
+  }
 }
 
 export default Home;
