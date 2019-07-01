d715b9e97c0ba7039ae0b3c042b8d6edd1c78716
Jordan Skophammer <jordan.skophammer@gmail.com>
2019-06-30 19:47:01 -0500

    removed router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route} from "react-router-dom";
+// import { BrowserRouter, Route} from "react-router-dom";
 import Home from "./pages/Home2";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Route exact path="/" render={() => <Home />} />
-      </BrowserRouter>
+      <Home />
     );
   }
 }
