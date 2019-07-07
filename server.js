7cc8f9ae62205760c44e59a19aa0204118249af8
Jordan Skophammer <jordan.skophammer@gmail.com>
2019-07-07 15:55:25 -0500

    server changes

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(cors())
 const router = express.Router();
 
 
-// app.use(express.static(path.join(__dirname, 'client/')))
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/client/public/index.html'))
-// })
+app.use(express.static(path.join(__dirname, 'client/')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'))
+})
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
